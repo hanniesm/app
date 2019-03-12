0d75ac63e1af9731eb087eb3584574280c6da4a1
Hannah <hanniesutton@gmail.com>
2019-03-12 18:40:55 +0000

    print urls to html

--- express_server.js
+++ express_server.js
@@ -21,6 +21,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// app.get("/urls", (req, res) => {
+//   let templateVars = { greeting: 'Hello World' };
+//   res.render("hello_world", templateVars);
+// })
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
