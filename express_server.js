e17e953f7a410169444d3cea4fad15f127c93a3d
Hannah <hanniesutton@gmail.com>
2019-03-13 00:08:35 +0000

    added some comments saying what the different gets do

--- express_server.js
+++ express_server.js
@@ -12,6 +12,7 @@ var urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//this function generates the shortURL
 function generateRandomstring() {
   return Math.random().toString(36).substr(2,5);
 }
@@ -38,18 +39,31 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+//this loads the urls/new page which has the form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//this loads the urls/:id pages
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 })
 
+//this will be used to redirect the short URL to the long URL
+app.get("/u/:shortURL" , (req, res) => {
+ let longURL = urlDatabase[req.params.shortURL];
+ res.redirect(`${longURL}`)
+})
+
+//this is used when the form is filled in. It redirects to /url/:shortID
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomstring()
+  urlDatabase[shortURL] = longURL;
+  // console.log(urlDatabase)  // Log the POST request body to the console
+ res.redirect(`/urls/${shortURL}`);
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
